429aa434998f3b6917a49d358bb72a8b94a79aae
sklbz <sokolbozanic@gmail.com>
2024-11-12 19:11:31 +0100

    working on the button

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,12 @@
 module.exports = {
+  syntax: 'postcss-scss',
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
     cssnano: {},
     "postcss-preset-env": {
       stage: 1,
+      features: {}
     },
   },
 };
--- src/components/gradient-button/gradient-button.tsx
+++ src/components/gradient-button/gradient-button.tsx
@@ -1,5 +1,5 @@
 import { component$, Slot } from "@builder.io/qwik";
-import styles from "./gradient-button.module.css";
+import styles from "./gradient-button.module.scss";
 
 export const GradientButton = component$(() => {
   return (
